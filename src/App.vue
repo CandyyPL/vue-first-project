aff249c3bbc2f9fc282101ea2928fc5844bfd389
CandyyPL <aplay58.yt@gmail.com>
2021-01-31 19:03:03 +0100

    Temp commit

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
   <div class="app">
     <div class="nav">
       <router-link to="/">spacer</router-link>
-      <router-link to="/about">about</router-link>
     </div>
     <router-view />
   </div>
---
+++ src/components/Claim.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1 class='claim'>SPACER</h1>
+        <p>
+            Begin your journey through our amazing galaxy,
+            and discover places you never even heard of.
+        </p>
+        <p>
+            Type anything space-related to start.
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style lang='scss' scoped>
+
+</style>
